3f7c0c9fa3cf50149a02e048254d86e5fee08458
25557 <43168595+25557@users.noreply.github.com>
2019-04-26 23:26:16 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php function themaTinxian_bronnen() { ?>
+<?php wp_enqueue_script('tinxian-scripts', get_theme_file_uri('/js/scriptsTinxian.js'), NULL, '1.0', true); ?>
 <?php wp_enqueue_style('style', get_stylesheet_uri()); ?>
 <?php } ?>
 
---
+++ js/scriptsTinxian.js
@@ -0,0 +1,18 @@
+// script voor submenu
+
+const heeftSubmenu = document.querySelectorAll('.menu-item-has-children');
+
+for (let i = 0; i < heeftSubmenu.length; i++) {
+  const submenu = heeftSubmenu[i].querySelector('.sub-menu');
+  const linkSubmenu = heeftSubmenu[i].querySelector('a');
+
+  // eerst het submenu met script verbergen, voorbeeld van graceful degradation
+
+  submenu.classList.toggle('menu-verbergen');
+
+  linkSubmenu.addEventListener('click', e => {
+    e.preventDefault();
+    submenu.classList.toggle('menu-verbergen');
+    linkSubmenu.classList.toggle('menu-is-open');
+  })
+}
